a45bae56dac1b0f5d9b4895e9d36219ea74f55d7
sovisrushain <sovisrushain@gmail.com>
2022-07-24 12:49:52 +0530

    04 lambda

--- exp-lambda/src/demo01/Main.java
+++ exp-lambda/src/demo01/Main.java
@@ -3,13 +3,17 @@ package demo01;
 public class Main {
     public static void main(String[] args) {
         Cat myCat = new Cat();
-        //myCat.print();
-        //printThing(myCat);
-        printThing(
-            () -> {
-                System.out.println("Meaw...");
-            }
-        );
+        // 01. we implement the Printable interface inside the Cat class
+        // 02. we define the print() method that we wanted inside that Cat class
+        // 03. create an object from that class
+        // 04. pass that cat object into PrintThing() method.
+
+        // ** 05. instead doing all that we pass the implementation of that print()
+        // method that we wanted to use as our printable parameter.
+
+        // ** Earlier we passed object with action
+        // ** Now we pass only the action
+        printThing(() -> System.out.println("Meaw..."));
     }
 
     // takes any object that implements Printable interface
